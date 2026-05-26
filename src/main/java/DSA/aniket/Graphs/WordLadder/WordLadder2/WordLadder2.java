265f452d71b21bc6caa0388788278e6efe9e44c7
Aniket Khangar <aniketkhangar459@gmail.com>
2026-05-26 10:02:48 +0530

    patch

--- src/main/java/DSA/aniket/Graphs/WordLadder/WordLadder2/WordLadder2.java
+++ src/main/java/DSA/aniket/Graphs/WordLadder/WordLadder2/WordLadder2.java
@@ -3,5 +3,12 @@ package DSA.aniket.Graphs.WordLadder.WordLadder2;
 import java.util.List;
 
 public interface WordLadder2 {
+    /**
+     *
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
     List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList);
 }
